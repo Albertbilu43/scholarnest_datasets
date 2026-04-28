e7cc373d4975afb79f55e073a55f090f42dbe878
Humberto Bizarro Luvian <albertbilu29@gmail.com>
2026-04-27 20:33:35 -0600

    Add files via upload

---
+++ spark_programmingHBL/INSTRUCTIONS GITHUB.py
@@ -0,0 +1,46 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 1 Create new repository
+# MAGIC
+# MAGIC     1 -  Go to GitHub/Repositories/
+# MAGIC     2 -  Click on New(the green button)
+# MAGIC        2 Name it "scholarnest_datasets"
+# MAGIC        3 Create Repository 
+# MAGIC
+# MAGIC 2 Upload a folder in a repository
+# MAGIC
+# MAGIC     1 - If the repository is empty, to start uploading files you first need to create a codespace.
+# MAGIC         - Click on Create a Codespace (in the section "Start coding with Codespaces" )
+# MAGIC         - Click on Create New Codespace 
+# MAGIC         - Return to your Homepage of GitHub
+# MAGIC     2 -  Go to GitHub/Repositories/
+# MAGIC     3 -  Select the repository "scholarnest_datasets"
+# MAGIC          Click on Add a file
+# MAGIC          Click on Upload files
+# MAGIC          Click on  Choose your files
+# MAGIC          Drag and drop the desired folder (spark_programmingHBL) to the repository space, once all files are complete  click cancel that popup window 
+# MAGIC          Click on Commit changes
+# MAGIC
+# MAGIC          If teh folder/file already exists then it will be replaced but you can delete it as it's mentioned in 4 Deleting....
+# MAGIC
+# MAGIC
+# MAGIC 3 Delete repository
+# MAGIC
+# MAGIC     1 -  Go to GitHub/Repositories/
+# MAGIC     2 -  Click on Settings
+# MAGIC        1 Delete Create Repository 
+# MAGIC        2 It will ask you to confirm this action
+# MAGIC
+# MAGIC
+# MAGIC 4 Deleting a directory/folder
+# MAGIC
+# MAGIC     1 -  Select the directory/folder
+# MAGIC     2 -  More Options (top-right)
+# MAGIC     3 -  Delete Directory
+# MAGIC     4 -  Commit changes
+# MAGIC
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
